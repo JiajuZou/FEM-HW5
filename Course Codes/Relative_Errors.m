b3c3239cd7590f05bc2160fbc4a0efca8f83603d
Zou Jiaju <jiajuzou@gmail.com>
2023-11-22 13:32:58 +0800

    使用quadratic element计算误差

--- Course Codes/Relative_Errors.m
+++ Course Codes/Relative_Errors.m
@@ -21,18 +21,19 @@ resultTable_H1 = table();
 
 
 for n_el = 2:2:16 % generate different mesh
-    hh = 1 / n_el;
+    hh = 1 / n_el / 2;
     x_coor = 0 : hh : 1;
 
     % IEN
-    IEN = zeros(2, n_el);
+    IEN = zeros(3, n_el);
     for ee = 1 : n_el
-        IEN(1,ee) = ee;
-        IEN(2,ee) = ee+1;
+        IEN(1,ee) = 2*ee - 1;
+        IEN(2,ee) = 2*ee;
+        IEN(3,ee) = 2*ee + 1;
     end
 
     % ID
-    n_pt = n_el + 1; % number of points
+    n_pt = 2*n_el + 1; % number of points
     ID = 1 : n_pt;
     ID(end) = 0;
 
@@ -43,67 +44,67 @@ for n_el = 2:2:16 % generate different mesh
     [xi, weight] = Gauss(n_int, -1, 1);
     
     
-   %% Assembly of K and F and solve Kd=F
-    K = zeros(n_eq,n_eq);
-    F = zeros(n_eq,1);
-    
-    for ee = 1 : n_el
-
-        k_e = zeros(2,2); %2*2 k matrix
-        f_e = zeros(2,1);
-
-        x_ele = zeros(2,1);
-        for aa = 1 : 2
-            x_ele(aa) = x_coor(IEN(aa,ee)); % A = IEN(a,e)
-        end
-
-    for l = 1 : n_int
-        dx_dxi = 0.0;
-        x_l = 0.0;
-        for aa = 1 : 2
-            dx_dxi = dx_dxi + x_ele(aa) * PolyShape(aa, xi(l), 1);
-            x_l = x_l + x_ele(aa) * PolyShape(aa, xi(l), 0);
-        end
-        dxi_dx = 1.0 / dx_dxi;
-
-        for aa = 1 : 2
-            for bb = 1 : 2
-                k_e(aa,bb) = k_e(aa,bb) + weight(l) * PolyShape(aa, xi(l), 1) * PolyShape(bb, xi(l), 1) * dxi_dx;
-            end
-        end
-
-        for aa = 1 : 2
-            f_e(aa) = f_e(aa) + weight(l) * PolyShape(aa, xi(l), 0) * f(x_l) * dx_dxi;
-        end
-
-    end
-
-    % Now we need to put element k and f into global K and F
-    for aa = 1 : 2
-        AA = IEN(aa,ee);
-        PP = ID(AA);
-        if PP > 0
-            F(PP) = F(PP) + f_e(aa);
-            for bb = 1 : 2
-                BB = IEN(bb,ee);
-                QQ = ID(BB);
-                if QQ > 0
-                    K(PP,QQ) = K(PP,QQ) + k_e(aa,bb);
-                else
-                    F(PP) = F(PP) - k_e(aa,bb) * g;
-                end
-            end
-        end
-    end
-
-        if ee == 1
-        F(ID(IEN(1,ee))) = F(ID(IEN(1,ee))) + h;
-        end
-    end
-% Now we have K and F
-% Solve Kd = F
-uh = K \ F;
-d = [uh; g];
+%    %% Assembly of K and F and solve Kd=F
+%     K = zeros(n_eq,n_eq);
+%     F = zeros(n_eq,1);
+%     
+%     for ee = 1 : n_el
+% 
+%         k_e = zeros(2,2); %2*2 k matrix
+%         f_e = zeros(2,1);
+% 
+%         x_ele = zeros(2,1);
+%         for aa = 1 : 2
+%             x_ele(aa) = x_coor(IEN(aa,ee)); % A = IEN(a,e)
+%         end
+% 
+%     for l = 1 : n_int
+%         dx_dxi = 0.0;
+%         x_l = 0.0;
+%         for aa = 1 : 2
+%             dx_dxi = dx_dxi + x_ele(aa) * PolyShape(aa, xi(l), 1);
+%             x_l = x_l + x_ele(aa) * PolyShape(aa, xi(l), 0);
+%         end
+%         dxi_dx = 1.0 / dx_dxi;
+% 
+%         for aa = 1 : 2
+%             for bb = 1 : 2
+%                 k_e(aa,bb) = k_e(aa,bb) + weight(l) * PolyShape(aa, xi(l), 1) * PolyShape(bb, xi(l), 1) * dxi_dx;
+%             end
+%         end
+% 
+%         for aa = 1 : 2
+%             f_e(aa) = f_e(aa) + weight(l) * PolyShape(aa, xi(l), 0) * f(x_l) * dx_dxi;
+%         end
+% 
+%     end
+% 
+%     % Now we need to put element k and f into global K and F
+%     for aa = 1 : 2
+%         AA = IEN(aa,ee);
+%         PP = ID(AA);
+%         if PP > 0
+%             F(PP) = F(PP) + f_e(aa);
+%             for bb = 1 : 2
+%                 BB = IEN(bb,ee);
+%                 QQ = ID(BB);
+%                 if QQ > 0
+%                     K(PP,QQ) = K(PP,QQ) + k_e(aa,bb);
+%                 else
+%                     F(PP) = F(PP) - k_e(aa,bb) * g;
+%                 end
+%             end
+%         end
+%     end
+% 
+%         if ee == 1
+%         F(ID(IEN(1,ee))) = F(ID(IEN(1,ee))) + h;
+%         end
+%     end
+% % Now we have K and F
+% % Solve Kd = F
+% uh = K \ F;
+% d = [uh; g];
 
 
     %% Calculate the Error L2 and H1
@@ -111,8 +112,8 @@ d = [uh; g];
     Error_H1 = 0.0;
     for ee = 1 : n_el
         
-        x_ele = zeros(2,1); % one element with two points
-        for aa = 1 : 2
+        x_ele = zeros(3,1); % one element with three points
+        for aa = 1 : 3
             x_ele(aa) = x_coor(IEN(aa,ee)); % A = IEN(a,e)
         end
        
@@ -124,11 +125,11 @@ d = [uh; g];
             x_l = 0.0;
             dx_dxi = 0.0;
             
-            for aa = 1 : 2
+            for aa = 1 : 3
                 dx_dxi = dx_dxi + x_ele(aa) * PolyShape(aa, xi(l), 1);
             end
             
-            for aa = 1 : 2
+            for aa = 1 : 3
                 u_h = u_h + exact(x_ele(aa)) * PolyShape(aa, xi(l), 0); 
                 %u_h = u_h + d(IEN(aa,ee)) * PolyShape(aa, xi(l), 0); 
                 %in the above equation, "exact(x_ele(aa))" is equal to
